a63cb7328b52fb5b759decc60cf4da8b0015f29b
Day2DayDev <jonathandivinaramos@gmail.com>
2023-04-06 14:51:04 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+//Steps are in reference to OneNote and ReadMe documentation. Cotinued from Step 9(Backend)
 
-function App() {
+//Step 9. Setting up the Client (FE) - App.js//
+import './App.css';
+import React, {useState} from "react"
+import Axios from "axios"
+
+
+function App(){
+  //Step 10. Creating the state variables //
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
+  const [registerStatus, setRegisterStatus] = useState("");
+
+  
+
+//Step 12. Create the registration and login function. Note that these functions are called in the button "onclick" within the forms //
+  const register = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3001/register", {
+      email: email,
+      username: username,
+      password: password,
+    }).then((response) => {
+      // setRegisterStatus(response);
+      // console.log(response);
+      if(response.data.message){
+        setRegisterStatus(response.data.message);
+      }else{
+        setRegisterStatus("ACCOUNT CREATED SUCCESSFULLY");
+      }
+    })
+  }
+
+  const login = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3001/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if(response.data.message){
+        setLoginStatus(response.data.message);
+      }else{
+        setLoginStatus(response.data[0].email);
+      }
+    })
+  }
+
+
+  //Step 11. Below is a copy/paste of a typical login/registration form. No need to reinvent the wheel!//
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+    <div className="loginForm">
+      <form>
+        <h4>Login Here</h4>
+        <label htmlFor="username">Username*</label>
+        <input className="textInput" type="text" name="username" onChange={(e) => {setUsername(e.target.value)}} placeholder="Enter your Username" required />
+        <label htmlFor="password">Password*</label>
+        <input className="textInput" type="password" name="password" onChange={(e) => {setPassword(e.target.value)}} placeholder="Enter your Password" required />
+        <input className="button" type="submit" onClick={login} value="Login" />
+        <h1 style={{color: 'red', fontSize: '15px', textAlign: 'center', marginTop: '20px'}}>{loginStatus}</h1>
+      </form>
     </div>
-  );
+
+    <div className="registrationForm">
+      <form>
+        <h4>Register Here</h4>
+        <label htmlFor="email">Email Address*</label>
+        <input className="textInput" type="text" name="email" onChange={(e) => {setEmail(e.target.value)}} placeholder="Enter your Email Address" required />
+        <label htmlFor="username">Username*</label>
+        <input className="textInput" type="username" name="username" onChange={(e) => {setUsername(e.target.value)}} placeholder="Enter your Username" required />
+        <label htmlFor="password">Password*</label>
+        <input className="textInput" type="password" name="password" onChange={(e) => {setPassword(e.target.value)}} placeholder="Enter your Password" required />
+        <input className="button" type="submit" onClick={register} value="Create an account" />
+        <h1 style={{fontSize: '15px', textAlign: 'center', marginTop: '20px'}}>{registerStatus}</h1>
+      </form>
+    </div>
+  </div>
+);
 }
 
 export default App;
